252efd5db4c0f3a76a3f951da00a54a6f5fae960
Rubens Araújo <rubens@octosoft.com.br>
2021-03-24 21:02:18 -0300

    Criação da API .Net 5

---
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Evento>>> Get(
+            [FromServices]DataContext context
+        )
+        {
+            var eventos = await context.Eventos.AsNoTracking().ToListAsync();
+
+            return Ok(eventos);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Evento>> GetById(
+            int id, 
+            [FromServices]DataContext context
+        )
+        {
+            var evento = await context.Eventos.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            if(evento == null)
+                return NotFound(new { message = "Categoria não encontrada!"});
+
+            return Ok(evento);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public string Post()
+        {
+            return "Método POST";
+        }
+
+
+
+    }
+}
